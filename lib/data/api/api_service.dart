8aaf77b607b3b47790b4aa5589e9254347d39e08
Muammarbanana <muammarfanani2@gmail.com>
2021-12-27 20:40:51 +0700

    add no connection handler

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter_resto_dicoding/data/models/resto_detail_model.dart';
 import 'package:flutter_resto_dicoding/data/models/resto_model.dart';
@@ -9,32 +10,44 @@ class ApiService {
   final String _baseUrl = 'https://restaurant-api.dicoding.dev/';
 
   Future<Resto> fetchRestoList() async {
-    final response = await http.get(Uri.parse(_baseUrl + 'list'));
-    if (response.statusCode == 200) {
-      return Resto.fromJson(json.decode(response.body));
-    } else {
-      throw Exception("Gagal mengambil data daftar restoran");
+    try {
+      final response = await http.get(Uri.parse(_baseUrl + 'list'));
+      if (response.statusCode == 200) {
+        return Resto.fromJson(json.decode(response.body));
+      } else {
+        throw ("Gagal mengambil data daftar restoran");
+      }
+    } on SocketException catch (_) {
+      throw ("Tidak dapat terhubung ke internet");
     }
   }
 
   Future<RestoDetailModel> fetchRestoDetail(String restoId) async {
-    final response = await http.get(Uri.parse(_baseUrl + 'detail/$restoId'));
-    if (response.statusCode == 200) {
-      return RestoDetailModel.fromJson(json.decode(response.body));
-    } else {
-      throw Exception("Gagal mengambil data detail restoran");
+    try {
+      final response = await http.get(Uri.parse(_baseUrl + 'detail/$restoId'));
+      if (response.statusCode == 200) {
+        return RestoDetailModel.fromJson(json.decode(response.body));
+      } else {
+        throw Exception("Gagal mengambil data detail restoran");
+      }
+    } on SocketException catch (_) {
+      throw ("Tidak dapat terhubung ke internet");
     }
   }
 
   Future<dynamic> fetchSearchedResto(String searchKeyword) async {
-    final response =
-        await http.get(Uri.parse(_baseUrl + 'search?q=$searchKeyword'));
-    if (response.statusCode == 200) {
-      return RestoSearchModel.fromJson(json.decode(response.body));
-    } else if (searchKeyword.isEmpty || searchKeyword == '') {
-      return fetchRestoList();
-    } else {
-      throw Exception('Gagal mengambil data pencarian');
+    try {
+      final response =
+          await http.get(Uri.parse(_baseUrl + 'search?q=$searchKeyword'));
+      if (response.statusCode == 200) {
+        return RestoSearchModel.fromJson(json.decode(response.body));
+      } else if (searchKeyword.isEmpty || searchKeyword == '') {
+        return fetchRestoList();
+      } else {
+        throw ('Gagal mengambil data pencarian');
+      }
+    } on SocketException catch (_) {
+      throw ('Tidak dapat terhubung ke internet');
     }
   }
 }
